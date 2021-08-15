deef7a6d31df11d23c574cdd11cacbdf8f9b589c
Shreya Sheel Sachan <85427961+sachan-shreya10@users.noreply.github.com>
2021-08-15 18:25:39 +0530

    Update App.js

--- Milestone #3/Greeting site/App.js
+++ Milestone #3/Greeting site/App.js
@@ -1,1 +1,37 @@
+import './App.css';
+let curdate=new Date();
+curdate=curdate.getHours();
+let greet='';
+const obj={};
+if(curdate>=3 && curdate<=11)
+{
+  greet='Good Morning!';
+  obj.color="green";
+}
+else if(curdate>11 && curdate<=16) 
+{
+  greet='Good Afternoon!';
+  obj.color="orange";
+}
+else if(curdate>16 && curdate<=19)
+{
+  greet='Good Evening!';  
+  obj.color="blue";
+}
+else
+{
+  greet='Good Night!';
+  obj.color="black";  
+}
+function App() {
+  return (
+    <>
+    <div> 
+    <h1>Hello Shreya,<span style={obj}>{greet}</span></h1>
+    </div>
+    </>
+  );
+}
+
+export default App;
 
