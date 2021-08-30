089b323f15953c800bdc64b016c9af6e2fa0a80c
Shreya Sheel Sachan <85427961+sachan-shreya10@users.noreply.github.com>
2021-08-30 19:58:46 +0530

    Create index.js

---
+++ Milestone #4/components/header/web/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./web.css";
+function Web() {
+    return (<div className="web">
+        <div className="web-option">
+            <a href="#projects">
+                Projects
+            </a>
+        </div>
+        <div className="web-option">
+            <a href="#skills">
+                Skills
+            </a>
+        </div>
+        <div className="web-option">
+            <a href="#contact">
+                Contact
+            </a>
+        </div>
+    </div>
+    );
+}
+
+export default Web;
