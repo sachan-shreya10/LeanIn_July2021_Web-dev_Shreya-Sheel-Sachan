5402ec338e9da4a3d1a23afd3b202fbbcca9d76f
Shreya Sheel Sachan <85427961+sachan-shreya10@users.noreply.github.com>
2021-08-30 19:50:34 +0530

    Create project.js

---
+++ Milestone #4/components/data/project.js
@@ -0,0 +1,18 @@
+export const ProjectData=[
+    {
+        id:1,
+        title:"Sorting Visualizer",
+        about:"A simple visualization project made using HTML, CSS , JAVASCRIPT It allows the users to select the sorting algorithm, array size and speed of visualization",
+        demo:" https://visualize-sorting-by-shreya.netlify.app/",
+        github:"https://github.com/sachan-shreya10/Sorting-Visualizer",
+        image:require("./sort.png").default,
+    },
+    {
+        id:2,
+        title:"Weather App",
+        about:"A weather application built using HTML, CSS, JAVASCRIPT It is built using open weather API for fetching weather and geolocation information and has the functionality of searchable cities.",
+        demo:"",
+        github:"https://github.com/sachan-shreya10/Weather-App",
+        image:require("./weather.png").default,
+    }
+];
