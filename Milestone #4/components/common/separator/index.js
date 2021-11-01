58b3cbb54e0c30247923cd7c5105091cc87b8a1c
Shreya Sheel Sachan <85427961+sachan-shreya10@users.noreply.github.com>
2021-11-01 14:54:27 +0530

    Delete Milestone #4 directory

--- Milestone #4/components/common/separator/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./sep.css";
-function Separator() 
-{
-    return <div className="sep"></div>
-}
-  
-  export default Separator;
--- Milestone #4/components/footer/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./footer.css";
-function Footer() 
-{
-    return <div className="footer">Created by Shreya. © 2021</div>
-}
-  
-  export default Footer;
